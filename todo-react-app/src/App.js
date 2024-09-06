f9fab4aadfe1bf0867a6fcbc90cec29922266321
ChangHyun <19941128o@gmail.com>
2024-09-06 19:45:17 +0900

    commit

---
+++ todo-react-app/src/App.js
@@ -0,0 +1,27 @@
+import './App.css';
+import Todo from './Todo';
+import React,{useState} from 'react';
+
+function App() {
+  const [items,setitems] = useState(
+    [{
+    id: "0",
+    title: "Hello World 1",
+    done: true 
+    },
+    {
+      id: "1",
+      title: "Hello World 2",
+      done: true,
+    }]);
+
+  let todoItems = items.length > 0 && items.map((item) => <Todo item={item} key={item.id} />)
+
+  return (
+    <div className="App">
+        {todoItems}
+    </div>
+  );
+}
+
+export default App;
---
+++ todo-react-app/src/Todo.js
@@ -0,0 +1,20 @@
+import React ,{ useState } from "react";
+
+const Todo = (props) => {
+    const [item, setItem] = useState(props.item); 
+
+    return (
+        <div className="Todo">
+            <input 
+            type="checkbox" 
+            id="{item.id}" 
+            name="{item.id}" 
+            value="{item.done}" 
+            />
+            <label id={item.id}>{item.title}</label>
+        </div>
+    );
+};
+
+
+export default Todo;
